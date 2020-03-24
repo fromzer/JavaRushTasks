15fed3ef59919d59c19368e734244c8970f13885
egor.mikheev <fromzer@mail.ru>
2020-03-24 15:49:34 +0300

    JavaRush

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task17.task1710;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /* 
 CRUD
@@ -16,7 +19,38 @@ public class Solution {
         allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         //start here - начни тут
+        SimpleDateFormat oldTime = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        SimpleDateFormat newTime = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+
+        switch (args[0]) {
+            case ("-c"):
+                if (args[2].equals("м")) {
+                    allPeople.add(Person.createMale(args[1], oldTime.parse(args[3])));
+                    System.out.println(allPeople.size() - 1); }
+                else if (args[2].equals("ж")) {
+                    allPeople.add(Person.createFemale(args[1], oldTime.parse(args[3])));
+                    System.out.println(allPeople.size() - 1); }
+                break;
+            case ("-u"):
+                if (args[3].equals("м"))
+                    allPeople.set(Integer.parseInt(args[1]), Person.createMale(args[2], oldTime.parse(args[4])));
+                else if (args[3].equals("ж"))
+                    allPeople.set(Integer.parseInt(args[1]), Person.createFemale(args[2], oldTime.parse(args[4])));
+                break;
+            case ("-d"):
+                int id = Integer.parseInt(args[1]);
+                allPeople.get(id).setName(null);
+                allPeople.get(id).setSex(null);
+                allPeople.get(id).setBirthDate(null);
+                break;
+            case ("-i"):
+                Person person = allPeople.get(Integer.parseInt(args[1]));
+                System.out.println(person.getName() + " " + (person.getSex().equals(Sex.MALE)? "м" : "ж") + " " + newTime.format(person.getBirthDate()));
+                break;
+            }
+
+
     }
 }
