abb5048fda24a7fc71410a592efe4a8aabce9c2d
egor.mikheev <fromzer@mail.ru>
2020-03-26 17:04:19 +0300

    JavaRush

--- 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -4,7 +4,28 @@ package com.javarush.task.task18.task1822;
 Поиск данных внутри файла
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new FileReader(consoleReader.readLine()));
+        int id = Integer.parseInt(args[0]);
+        while (reader.ready()) {
+            String str = reader.readLine();
+            String[] resStr = str.split(" ");
+            int strId = 0;
+            try {
+                strId = Integer.parseInt(resStr[0]);   
+            } catch (NumberFormatException e) {}
+            if (strId == id) {
+                System.out.println(str);
+            }
+        }
+        reader.close();
+        consoleReader.close();
     }
 }
