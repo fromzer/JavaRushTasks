cb453aa4a8272f7de2252da0cdf9289ce46996c0
egor.mikheev <fromzer@mail.ru>
2020-04-03 15:32:31 +0300

    JavaRush

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task20.task2003;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/*
+Знакомство с properties
+*/
+public class Solution {
+    public static Map<String, String> properties = new HashMap<>();
+
+    public void fillInPropertiesMap() throws Exception {
+        //implement this method - реализуйте этот метод
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream inputStream = new FileInputStream(br.readLine());
+        load(inputStream);
+        inputStream.close();
+    }
+
+    public void save(OutputStream outputStream) throws Exception {
+        //implement this method - реализуйте этот метод
+        Properties prop = new Properties();
+        prop.putAll(properties);
+        prop.store(outputStream, "my comments");
+    }
+
+    public void load(InputStream inputStream) throws Exception {
+        //implement this method - реализуйте этот метод
+        Properties prop = new Properties();
+        prop.load(inputStream);
+        properties.putAll((Map)prop);
+    }
+
+    public static void main(String[] args) {
+    }
+}
