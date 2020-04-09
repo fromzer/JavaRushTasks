c0cdc954608578173bbbc4d7bf3424b646195f32
egor.mikheev <fromzer@mail.ru>
2020-04-09 15:02:27 +0300

    JavaRush

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3213/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task32.task3213;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+/* 
+Шифр Цезаря
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringReader reader = new StringReader("Khoor#Dpljr#&C,₷B'3");
+        System.out.println(decode(reader, -3));  //Hello Amigo #@)₴?$0
+    }
+
+    public static String decode(StringReader reader, int key) throws IOException {
+        if (reader == null)
+            return new String();
+
+        String result = "";
+        int count;
+        while ((count = reader.read()) != -1) {
+            count = count + key;
+            result += (char) count;
+        }
+        return result;
+    }
+}
