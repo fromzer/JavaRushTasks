de60f7ea14ac23cc744ce63d41cd39c1c26ed43d
egor.mikheev <fromzer@mail.ru>
2020-04-16 15:53:08 +0300

    JavaRush

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
@@ -7,6 +7,26 @@ import java.util.List;
 public class ModelData {
     private List<User> users;
     private List<User> deletedUsers;
+    private User activeUser;
+    private boolean displayDeletedUserList;
+
+    public boolean isDisplayDeletedUserList() {
+        return displayDeletedUserList;
+    }
+
+    public void setDisplayDeletedUserList(boolean displayDeletedUserList) {
+        this.displayDeletedUserList = displayDeletedUserList;
+    }
+
+    public User getActiveUser() {
+
+        return activeUser;
+    }
+
+    public void setActiveUser(User activeUser) {
+        setDisplayDeletedUserList(true);
+        this.activeUser = activeUser;
+    }
 
     public List<User> getDeletedUsers() {
         return deletedUsers;
@@ -21,6 +41,7 @@ public class ModelData {
     }
 
     public void setUsers(List<User> users) {
+        setDisplayDeletedUserList(isDisplayDeletedUserList());
         this.users = users;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
@@ -4,19 +4,13 @@ import com.javarush.task.task36.task3608.bean.User;
 import com.javarush.task.task36.task3608.controller.Controller;
 import com.javarush.task.task36.task3608.model.ModelData;
 
-public class UsersView implements View {
+public class EditUserView implements View {
     private Controller controller = new Controller();
 
     @Override
     public void refresh(ModelData modelData) {
-        if (modelData.isDisplayDeletedUserList()) {
-            System.out.println("All deleted users:");
-        } else {
-            System.out.println("All users:");
-        }
-        for (User user : modelData.getUsers()) {
-            System.out.println("\t" + user.toString());
-        }
+        System.out.println("User to be edited:");
+        System.out.println("\t" + modelData.getActiveUser().toString());
         System.out.println("===================================================");
     }
 
@@ -33,3 +27,4 @@ public class UsersView implements View {
         this.controller = controller;
     }
 }
+
