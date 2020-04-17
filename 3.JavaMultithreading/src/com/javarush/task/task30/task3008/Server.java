63b1c00bd70d6e76af3c35be51c22a9754e5dfae
egor.mikheev <fromzer@mail.ru>
2020-04-17 10:11:09 +0300

    JavaRush

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,22 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message) {
+        for (String name: connectionMap.keySet()) {
+            try {
+                connectionMap.get(name).send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage(String.format("Can't send the message to %s", name));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
             System.out.println("Server has started");
