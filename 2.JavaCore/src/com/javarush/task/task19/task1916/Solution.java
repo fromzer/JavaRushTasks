3e183a0c6dff511e6d06645440adeac705262498
egor.mikheev <fromzer@mail.ru>
2020-04-20 16:05:34 +0300

    JavaRush

--- 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -10,38 +10,70 @@ import java.util.List;
 
 public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
+    public static ArrayList<String> f1 = new ArrayList<>();
+    public static ArrayList<String> f2 = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader file1 = new BufferedReader(new FileReader(bufferedReader.readLine()));
         BufferedReader file2 = new BufferedReader(new FileReader(bufferedReader.readLine()));
         bufferedReader.close();
-        boolean check = true;
 
-        while (check) {
-            String lineFile1 = "", lineFile2 = "";
-            if (file1.ready()) {
-                lineFile1 = file1.readLine();
-            }
-            if (file2.ready()) {
-                lineFile2 = file2.readLine();
-            }
+        writeInList(file1, f1);
+        writeInList(file2, f2);
 
-            if (lineFile1.equals(lineFile2)) {
-                lines.add(new LineItem(Type.SAME, lineFile1));
-            } else if (lineFile1.equals("")) {
-                lines.add(new LineItem(Type.ADDED, lineFile2));
-            } else if (lineFile2.equals("")) {
-                lines.add(new LineItem(Type.REMOVED, lineFile1));
+        for (int i = 0; i<f1.size() && i<f2.size();i++) {
+            try {
+                if (f1.get(i).equals(f2.get(i))) {
+                    lines.add(new LineItem(Type.SAME, f2.get(i)));
+                    f1.remove(i);
+                    f2.remove(i);
+                    i--;
+                } else if (!f1.get(i).equals(f2.get(i)) && !f1.get(i).equals(f2.get(i + 1))) {
+                    lines.add(new LineItem(Type.REMOVED, f1.get(i)));
+                    f1.remove(i);
+                    i--;
+                } else if (!f1.get(i).equals(f2.get(i)) && f1.get(i).equals(f2.get(i + 1))) {
+                    lines.add(new LineItem(Type.ADDED, f2.get(i)));
+                    f2.remove(i);
+                    i--;
+                }
             }
-
-            if (!file1.ready() && !file2.ready()) check = false;
+            catch (ArrayIndexOutOfBoundsException e) {
+                lines.add(new LineItem(Type.REMOVED, f1.get(i)));
+                f1.remove(i);
+                i--;
+            }
         }
+
+        while (!f1.isEmpty()) {
+            lines.add(new LineItem(Type.REMOVED, f1.get(0)));
+            f1.remove(0);
+        }
+        while (!f2.isEmpty()) {
+            lines.add(new LineItem(Type.ADDED, f2.get(0)));
+            f2.remove(0);
+        }
+
+        for (LineItem t : lines) {
+            System.out.println(t.line +" "+ t.type);
+        }
+
 
         file1.close();
         file2.close();
     }
 
+    public static Boolean trackСhanges(String line1, String line2) {
+
+        return true;
+    }
+
+    public static void writeInList(BufferedReader reader, ArrayList<String> list) throws IOException {
+        while (reader.ready()) {
+            list.add(reader.readLine());
+        }
+    }
 
     public static enum Type {
         ADDED,        //добавлена новая строка
